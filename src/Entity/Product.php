f6f1ba62e6702b4899f655f8b502fb03d0b67b70
Elhadj BAH <ismailabah16@gmail.com>
2022-02-02 22:05:02 +0100

    add groups annotation product:read

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,6 +6,7 @@ use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
@@ -16,26 +17,31 @@ class Product
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups("product:read")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("product:read")
      */
     private $name;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups("product:read")
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("product:read")
      */
     private $image;
 
     /**
      * @ORM\Column(type="float")
+     * @Groups("product:read")
      */
     private $price;
 
